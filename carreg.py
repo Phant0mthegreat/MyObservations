1e1a01153c67e612e9f04ab45daeb6c21cdbd085
Phant0m The Great <120599821+Phant0mthegreat@users.noreply.github.com>
2023-05-10 20:29:12 -0300

    Create carreg.py

---
+++ carreg.py
@@ -0,0 +1,105 @@
+import time
+
+import os
+
+import c
+
+def carreg():
+
+  os.system('clear')
+
+  print(f'''
+
+     _~
+
+   _~ )_)_~
+
+   )_))_))_)
+
+   _!__!__!_
+
+   \_______/
+
+  ~~~~~~~~~~~~~
+
+Entrando no {c.bwhite}MyObservations{c.white}...🔍''')
+
+  time.sleep(1)
+
+  os.system('clear')
+
+  print(f'''
+
+      _~
+
+    _~ )_)_~
+
+    )_))_))_)
+
+    _!__!__!_
+
+    \_______/
+
+  ~~~~~~~~~~~~~
+
+Entrando no {c.bwhite}MyObservations{c.white}...🔍''')
+
+  time.sleep(1)
+
+  os.system('clear')
+
+  print(f'''
+
+       _~
+
+     _~ )_)_~
+
+     )_))_))_)
+
+     _!__!__!_
+
+     \_______/
+
+  ~~~~~~~~~~~~~
+
+Entrando no {c.bwhite}MyObservations{c.white}...🔍''')
+
+  time.sleep(1)
+
+  os.system('clear')
+
+  print(f'''
+
+     _~
+
+   _~ )_)_~
+
+   )_))_))_)
+
+   _!__!__!_
+
+   \_______/
+
+  ~~~~~~~~~~~~~
+
+Entrando no {c.bwhite}MyObservations{c.white}...🔍''')
+
+  time.sleep(1)
+
+  os.system('clear')
+
+  print(f'''
+
+      _~
+
+    _~ )_)_~
+
+    )_))_))_)
+
+    _!__!__!_
+
+    \_______/
+
+  ~~~~~~~~~~~~~
+
+Entrando no {c.bwhite}MyObservations{c.white}...🔍''')
